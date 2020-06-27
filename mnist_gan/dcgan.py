76679be6cc6606f9435e8fefcbf02d635d2b5a5d
ecly <ecly@mailbox.org>
2020-06-28 00:06:12 +0200

    document, simplify and minimize dcgan.py

--- mnist_gan/dcgan.py
+++ mnist_gan/dcgan.py
@@ -1,5 +1,6 @@
 # pylint: disable=invalid-name,missing-docstring,missing-class-docstring,arguments-differ,C0330,too-many-locals
 """Partially based on: https://pytorch.org/tutorials/beginner/dcgan_faces_tutorial.html"""
+import os
 import argparse
 import torch
 import torch.nn as nn
@@ -11,10 +12,14 @@ import torchvision.utils as vutils
 import pytorch_lightning as pl
 
 
+_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_DATA_PATH = os.path.join(_FILE_PATH, "..", "data")
 IMAGE_SIZE = 32
 
 
 class Generator(nn.Module):
+    """Minimalistic Generator implementation"""
+
     def __init__(self, hparams):
         super(Generator, self).__init__()
         self.hparams = hparams
@@ -38,11 +43,13 @@ class Generator(nn.Module):
             # state size. (nc) x 32 x 32
         )
 
-    def forward(self, x):
-        return self.main(x)
+    def forward(self, z):
+        return self.main(z)
 
 
 class Discriminator(nn.Module):
+    """Minimalistic Discriminator implementation"""
+
     def __init__(self, hparams):
         super(Discriminator, self).__init__()
         self.hparams = hparams
@@ -71,64 +78,72 @@ class Discriminator(nn.Module):
 
 
 class DCGAN(pl.LightningModule):  # pylint: disable=too-many-ancestors
+    """A PyTorch Lightning wrapper for training G and D"""
+
     def __init__(self, args):
         super(DCGAN, self).__init__()
         self.hparams = args
         self.g = Generator(args)
         self.d = Discriminator(args)
         # use for generating samples showing progress over time
-        self.fixed_noise = torch.randn(64, args.nz, 1, 1)
+        self.fixed_z = torch.randn(64, args.nz, 1, 1)
         self.criterion = F.binary_cross_entropy
 
     def forward(self, x):
         return self.g(x)
 
-    def training_step(self, batch, _batch_idx, optimizer_idx):
-        real_x, _ = batch
-        b_size = real_x.size(0)
+    def _generator_step(self, batch):
+        """Definition of a single training step for G"""
+        b_size = batch[0].size(0)
         device = torch.device("cuda" if self.on_gpu else "cpu")
 
-        if optimizer_idx == 0:
-            ## Forward pass real batch through D
-            real_output = self.d(real_x).view(-1)
-            real_label = torch.ones((b_size,), device=device)
-            real_err = self.criterion(real_output, real_label)
-
-            ## Generate fake batch and forward pass through D
-            noise = torch.randn(b_size, self.hparams.nz, 1, 1, device=device)
-            fake_x = self.g(noise)
-            fake_label = torch.zeros((b_size,), device=device)
-            fake_output = self.d(fake_x.detach()).view(-1)
-            fake_err = self.criterion(fake_output, fake_label)
-
-            err_d = real_err + fake_err
-            log_dict = {"d_loss": err_d}
-            return {"loss": err_d, "progress_bar": log_dict, "log": log_dict}
-
         # If optimizer_idx != 0 we are training G
-        noise = torch.randn(b_size, self.hparams.nz, 1, 1, device=device)
-        fake_x = self.g(noise)
+        z = torch.randn(b_size, self.hparams.nz, 1, 1, device=device)
+        fake_x = self.g(z)
         gen_output = self.d(fake_x).view(-1)
         gen_label = torch.ones((b_size,), device=device)
         err_g = self.criterion(gen_output, gen_label)
         log_dict = {"g_loss": err_g}
+
         return {"loss": err_g, "progress_bar": log_dict, "log": log_dict}
+
+    def _discriminator_step(self, batch):
+        """Definition of a single training step for D"""
+        x, _ = batch
+        b_size = x.size(0)
+        device = torch.device("cuda" if self.on_gpu else "cpu")
+
+        ## Forward pass real batch through D
+        real_output = self.d(x).view(-1)
+        real_label = torch.ones((b_size,), device=device)
+        real_err = self.criterion(real_output, real_label)
+
+        ## Generate fake batch and forward pass through D
+        z = torch.randn(b_size, self.hparams.nz, 1, 1, device=device)
+        fake_x = self.g(z)
+        fake_label = torch.zeros((b_size,), device=device)
+        fake_output = self.d(fake_x.detach()).view(-1)
+        fake_err = self.criterion(fake_output, fake_label)
+
+        err_d = real_err + fake_err
+        log_dict = {"d_loss": err_d}
+        return {"loss": err_d, "progress_bar": log_dict, "log": log_dict}
+
+    def training_step(self, batch, _batch_idx, optimizer_idx):
+        """Training step definition for multi-optimizer PyTorch Lightning"""
+        if optimizer_idx == 0:
+            return self._discriminator_step(batch)
+
+        return self._generator_step(batch)
 
     def configure_optimizers(self):
         """For GANs we return two optimizer, order coupled with train step"""
-        optimizer_d = Adam(
-            self.d.parameters(),
-            lr=self.hparams.lr,
-            betas=(self.hparams.beta1, self.hparams.beta2),
-        )
-        optimizer_g = Adam(
-            self.g.parameters(),
-            lr=self.hparams.lr,
-            betas=(self.hparams.beta1, self.hparams.beta2),
-        )
+        optimizer_d = Adam(self.d.parameters(), lr=self.hparams.lr)
+        optimizer_g = Adam(self.g.parameters(), lr=self.hparams.lr)
         return [optimizer_d, optimizer_g], []
 
     def train_dataloader(self):
+        """Implicit PyTorch Lightning train dataloader definition"""
         train_set = datasets.MNIST(
             self.hparams.data,
             train=True,
@@ -137,7 +152,7 @@ class DCGAN(pl.LightningModule):  # pylint: disable=too-many-ancestors
                 [
                     transforms.Resize(IMAGE_SIZE),
                     transforms.ToTensor(),
-                    transforms.Normalize((0.5,), (0.5,)),
+                    transforms.Normalize(0.5, 0.5),
                 ]
             ),
         )
@@ -153,29 +168,26 @@ class DCGAN(pl.LightningModule):  # pylint: disable=too-many-ancestors
         After every epoch we generate and log our images on fixed noise
         to see progress on the same latent vector over time.
         """
-        z = self.fixed_noise
-
+        z = self.fixed_z
         if self.on_gpu:
             z = z.to(torch.device("cuda"))
 
-        y_pred = self.g(z)
-        grid = vutils.make_grid(y_pred)
+        fake_x = self.g(z)
+        grid = vutils.make_grid(fake_x)
         self.logger.experiment.add_image("generated_images", grid, self.current_epoch)
 
 
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", type=str, default="../data")
+    parser.add_argument("--data", type=str, default=DEFAULT_DATA_PATH)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--workers", type=int, default=4, help="workers for dataloader")
     parser.add_argument("--batch-size", type=int, default=256, help="mini batch size")
     parser.add_argument("--nc", type=int, default=1, help="number of channels in image")
     parser.add_argument("--nz", type=int, default=100, help="size of z (latent vector)")
-    parser.add_argument("--ngf", type=int, default=64, help="size of feature map in g")
-    parser.add_argument("--ndf", type=int, default=64, help="size of feature map in d")
+    parser.add_argument("--ngf", type=int, default=32, help="size of feature map in g")
+    parser.add_argument("--ndf", type=int, default=32, help="size of feature map in d")
     parser.add_argument("--lr", type=float, default=0.0002, help="learning rate")
-    parser.add_argument("--beta1", type=float, default=0.5, help="beta1 for Adam")
-    parser.add_argument("--beta2", type=float, default=0.999, help="beta1 for Adam")
     return parser
 
 
